d8ae72e48cb1a2d4f3ebf00018fe5407f8a98001
erf225 <99058060+erf225@users.noreply.github.com>
2025-03-19 15:51:36 -0400

    Update to connect to interface

--- keypad.test_top.sv
+++ keypad.test_top.sv
@@ -1,5 +1,5 @@
 module keypad_test_top;
   keypad_io top_io();
   keypad1_tb test(top_io);
-  keypad1 DUT(.clk(clk_tb), .en(en_tb), .rst(rst_tb), .row(row_tb), .col(col_tb), .access_granted(access_granted_tb), .led(led_tb));
+  keypad1 DUT(.clk(top_io.clk), .en(top_io.en), .rst(top_io.rst), .row(top_io.row), .col(top_io.col), .access_granted(top_io.access_granted), .led(top_io.led));
 endmodule
